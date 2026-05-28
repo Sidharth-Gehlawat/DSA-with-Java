5d9540dce50d51615035e8114390ba60f53e736c
Sidharth-Gehlawat <s.gehlawat56@gmail.com>
2026-05-28 08:49:56 +0530

    Create JavaBasics class with main method

---
+++ JavaBasics.java
@@ -0,0 +1,6 @@
+public class JavaBasics {
+    public static void main(String args[]) {
+        System.out.println("Welcome to my codespace.");
+        System.out.println("You can join me on this journey of revising DSA.");
+    }
+}
