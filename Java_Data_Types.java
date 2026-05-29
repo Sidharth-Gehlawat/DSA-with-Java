7556c6b37a36a5ef24c618d90e0d53fa53409e09
Sidharth-Gehlawat <s.gehlawat56@gmail.com>
2026-05-29 22:15:38 +0530

    Data Types in Java

---
+++ Java_Data_Types.java
@@ -0,0 +1,20 @@
+public class Java_Data_Types {
+   public static void main(String args[]) {
+      byte b = 8;
+      System.out.println(b);
+      char ch = 'a';
+      System.out.println(ch);
+      boolean var = false;
+      System.out.println(var);
+      float price = 10.5;
+      System.out.println(price);
+      int number = 25;
+      System.out.println(number);
+      long number1 = 2556780;
+      System.out.println(number1);
+      double price1 = 2354.8765;
+      System.out.println(price1);
+      short n = 240;
+      System.out.println(n);
+  }
+}
