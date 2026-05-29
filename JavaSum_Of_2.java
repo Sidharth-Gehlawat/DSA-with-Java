9d0f547ad43a9a343b2be17d78ed130f3e1ae93b
Sidharth-Gehlawat <s.gehlawat56@gmail.com>
2026-05-29 22:08:24 +0530

    Sum of two numbers

---
+++ JavaSum_Of_2.java
@@ -0,0 +1,8 @@
+public class JavaSum_Of_2 {
+   public static void main(String args[]) {
+      int a = 5;
+      int b = 10;
+      int sum = a + b;
+      System.out.println("The sum of a & b is: " + sum);
+   }
+}
