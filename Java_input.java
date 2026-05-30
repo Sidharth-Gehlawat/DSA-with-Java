addc4b3343551eb1e9a7e6ed8b26a829fe77b190
Sidharth-Gehlawat <s.gehlawat56@gmail.com>
2026-05-30 19:48:01 +0530

    Taking input in java

---
+++ Java_input.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Java_input {
+  public static void main(String args[]) {
+    Scanner sc = new scanner(System.in);
+    System.out.print("Enter your number: ");
+    int num = sc.nextInt();
+    System.out.print("You entered: " + num);
+    if (num < 10) {
+      System.out.println("Are you Sure?");
+    }else{
+      System.out.println("Thanks for using the program");
+    }
+  }
+}
+    
