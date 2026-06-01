6f975520e448911529e2cbef7055983d3fc7d176
Sidharth-Gehlawat <s.gehlawat56@gmail.com>
2026-06-01 23:18:33 +0530

    Add Student class and main method for display (OOPs)

---
+++ JavaClassObject.java
@@ -0,0 +1,19 @@
+class Student {
+    String name;
+    int age;
+
+    void display() {
+        System.out.println(name + " " + age);
+    }
+}
+
+public class JavaClassObject {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+
+        s1.name = "Sidharth";
+        s1.age = 20;
+
+        s1.display();
+    }
+}
