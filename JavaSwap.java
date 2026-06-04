543032a2f4797e1dcf78bdfe3804d241b816a633
Sidharth-Gehlawat <s.gehlawat56@gmail.com>
2026-06-04 23:45:49 +0530

    Swapping two numbers in java
    //Swap

---
+++ JavaSwap.java
@@ -0,0 +1,16 @@
+public class JavaSwap {
+  public static void main (String args[]) {
+    int a = 5;
+    int b = 10;
+
+    // now i will swap the value of a with b by introducing a temporary variable
+
+    int temp = a;
+    a = b;
+    b = temp;
+
+    System.out.println("a = " + a);
+    System.out.println("b = " + b);
+  }
+    System.out.println("Thanks for using my program");
+}
