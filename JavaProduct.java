d4e83b7f20e6cfe5f56987e2719c5e6732c0a3c7
Sidharth-Gehlawat <s.gehlawat56@gmail.com>
2026-06-04 23:41:14 +0530

    Product using method

---
+++ JavaProduct.java
@@ -0,0 +1,14 @@
+public class JavaProduct {
+  public static int multiply(int a, int b) {
+    int product = a * b;
+    return product;
+  }
+
+  public static void main(String args[]) {
+    int a = 2;
+    int b = 5;
+    int prod = multiply(3,5);
+    System.out.println(prod);
+  }
+    System.out.println("Thanks for using my program");
+}
