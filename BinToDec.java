8682776db93c2ffa5f4f61192c69e10abef91cc4
Sidharth-Gehlawat <s.gehlawat56@gmail.com>
2026-06-05 18:31:21 +0530

    Binary To Decimal Conversion

---
+++ BinToDec.java
@@ -0,0 +1,21 @@
+public class BinToDec {
+  public static void binToDec(int binNum) {
+    int myNum = binNum;
+    int pow = 0;
+    int decNum;
+
+    while(binNum > 0) {
+      int lastDigit = binNum % 10;
+      decNum = decNum + (lastDigit * (int)Math.pow(2,pow));
+
+      pow ++;
+      binNum = binNum / 10;
+    }
+
+    System.out.println("decimal of " +myNum + " =" + decNum);
+  }
+
+  public static void main(String args []) {
+    binToDec (101);
+  }
+}
