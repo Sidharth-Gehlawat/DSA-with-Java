026f901abd48b6abdae4354fdde78b9dffd852ed
Sidharth-Gehlawat <s.gehlawat56@gmail.com>
2026-06-10 22:58:45 +0530

    Simple Interest in java

---
+++ SimpleInterest.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class SimpleInterest {
+    public static void main(String args[])
+    {
+       
+        float P = 1, R = 1, T = 1;
+
+        /* Calculate simple interest */
+        float SI = (P * R * T) / 100;
+        System.out.println("Simple interest = " + SI);
+    }
+}
+
