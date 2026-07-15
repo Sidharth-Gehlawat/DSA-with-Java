b38deedd6586db7d8f9c6dbee218674b403ea12c
Sidharth-Gehlawat <s.gehlawat56@gmail.com>
2026-07-15 21:29:37 +0530

    Implement reverse linked list functionality

---
+++ ListNode.java
@@ -0,0 +1,20 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) { this.val = val; }
+}
+
+public class ReverseLinkedList {
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
+    }
+}
